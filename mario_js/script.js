58e273d129764f85e6f3bdf9b400086b256f50e5
Kelly Stewart <murrayt185@hotmail.com>
2025-04-22 12:06:57 +0100

    updated visuals

--- mario_js/script.js
+++ mario_js/script.js
@@ -1,4 +1,7 @@
-
+const bg_img_width = 2560;
+const bg_img_height = 1440
+const original_ground_height = 1247;
+const original_size = 50;
 // dom elements
 const form = document.querySelector("form");
 const input = document.getElementById("height-input");
@@ -9,42 +12,44 @@ const canvas_div = document.querySelector(".canvas-container");
 const img = document.getElementById("mario-block");
 const background = document.getElementById("mario-background");
 
-const canvas = document.createElement("canvas");
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
-console.log(canvas_div.hasChildNodes());
 
 // global variables
 const bg_color = "black";
+let ground_height = original_ground_height;
+let size = original_size;
+let scaling_factor = 0;
 let gap = 3;
 // initialise rect object
 let rect = {
     x: 1,
-    y: 10,
-    height: 25,
-    width: 25,
+    y: ground_height - size,
+    height: size,
+    width: size,
 }
 
 // event listeners
  less_button.addEventListener("click", (event) => {
-    if (!canvas_div.children.length){
-        canvas_div.appendChild(canvas);
-    }
-    canvas.width = 60 + (parseInt(input.value) * (rect.width * 2)) + (2 * parseInt(input.value) + (gap * rect.width) + gap);
-    canvas.height = (parseInt(input.value) * (rect.height)) + (gap * parseInt(input.value)) + 20;
+    // if (!canvas_div.children.length){
+    //     canvas_div.appendChild(canvas);
+    //     console.log("added canvas");
+    // }
+    // canvas.width = 60 + (parseInt(input.value) * (rect.width * 2)) + (2 * parseInt(input.value) + (gap * rect.width) + gap);
+    // canvas.height = (parseInt(input.value) * (rect.height)) + (gap * parseInt(input.value)) + 20;
     clear_canvas("red", true);
-    console.log("click");
+    console.log(rect);
     // ctx.drawImage(img, 10, 10, 50, 50);
     show_left_pyramid(input.value, rect, true)
  });
 
  more_button.addEventListener("click", (event) => {
-    if (!canvas_div.children.length){
-        canvas_div.appendChild(canvas);
-    }
+    // if (!canvas_div.children.length){
+    //     canvas_div.appendChild(canvas);
+    // }
         // dynamically set canvas width
-        canvas.width = 60 + (parseInt(input.value) * (rect.width * 2)) + (2 * parseInt(input.value) + (gap * rect.width) + gap);
-        canvas.height = (parseInt(input.value) * (rect.height)) + (gap * parseInt(input.value)) + 20;
+        // canvas.width = 60 + (parseInt(input.value) * (rect.width * 2)) + (2 * parseInt(input.value) + (gap * rect.width) + gap);
+        // canvas.height = (parseInt(input.value) * (rect.height)) + (gap * parseInt(input.value)) + 20;
         clear_canvas("red", true);
         rect = show_left_pyramid(input.value, rect, true);
         rect.y = 10;    // reset y
@@ -57,20 +62,17 @@ let rect = {
 form.addEventListener("submit", (event) => {
     event.preventDefault(); // Prevent the form from submitting the traditional way
 
-    if (!canvas_div.children.length){
-        canvas_div.appendChild(canvas);
-    }
+    // if (!canvas_div.children.length){
+    //     canvas_div.appendChild(canvas);
+    // }
 
     // dynamically set canvas width
-    canvas.width = 60 + (parseInt(input.value) * (rect.width * 2)) + (2 * parseInt(input.value) + (gap * rect.width) + gap);
-    canvas.height = (parseInt(input.value) * (rect.height)) + (gap * parseInt(input.value)) + 20;
+    // canvas.width = 60 + (parseInt(input.value) * (rect.width * 2)) + (2 * parseInt(input.value) + (gap * rect.width) + gap);
+    // canvas.height = (parseInt(input.value) * (rect.height)) + (gap * parseInt(input.value)) + 20;
     clear_canvas("red", true);
     rect = show_left_pyramid(input.value, rect);
-    rect.y = 10;    // reset y
     rect = show_gap(input.value, rect, gap);
-    rect.y = 10;    // reset y
     rect = show_right_pyramid(input.value, rect);
-    rect.y = 10;    // reset y
 });
 
 height_slider.oninput = function () {
@@ -80,9 +82,10 @@ height_slider.oninput = function () {
 
 function show_right_pyramid(n, rect, use_img=false, color="red"){
     let x;
-    for (let i = 0; i < n; i++){
+    rect.y = ground_height - size
+    for (let i = n; i >= 0; i--){
         x = rect.x;
-        for (let j = 0; j <= i; j++){
+        for (let j = 0; j < i; j++){
             if(use_img){
                 ctx.drawImage(img, x, rect.y, rect.height, rect.width);
             }
@@ -92,7 +95,7 @@ function show_right_pyramid(n, rect, use_img=false, color="red"){
             }
             x += rect.width;
         }
-        rect.y += rect.height;
+        rect.y -= rect.height;
     }
     rect.x = x;
     return rect;
@@ -100,10 +103,11 @@ function show_right_pyramid(n, rect, use_img=false, color="red"){
 
 function show_left_pyramid(n, rect, use_img=false,color="green"){
     let x;
-    rect.y = 10;
-    for (let i = 0; i < n; i++){
-        x = (n - i + 1) * rect.width;
-        for (let j = 0; j <= i; j++){
+    rect.y = ground_height - size
+    for (let i = n; i > 0; i--){
+        // puts center of gap in center of canvas
+        x = (n - i + 1) * rect.width + ((canvas.width / 2) - (size * n) - ((size * gap) / 2)) ;
+        for (let j = 0; j < i; j++){
             if(use_img){
                 ctx.drawImage(img, x, rect.y, rect.height, rect.width);
             }
@@ -113,7 +117,7 @@ function show_left_pyramid(n, rect, use_img=false,color="green"){
             }
             x += rect.width;
         }
-        rect.y += rect.height;
+        rect.y -= rect.height;
     }
     rect.x = x;
     return rect;
@@ -136,6 +140,20 @@ function show_gap(n, rect, gap, use_img=false, color=bg_color){
 }
 
 function clear_canvas(color=bg_color, img=false){
+    canvas.width = window.innerWidth - (window.innerWidth / 20);
+    scaling_factor = canvas.width / bg_img_width;
+    canvas.height = bg_img_height * scaling_factor;
+    ground_height = canvas.height - (canvas.height / (bg_img_height / 193));
+    size = original_size * scaling_factor
+
+    rect = {
+        x: 1,
+        y: ground_height - size,
+        height: size,
+        width: size,
+    }
+
+    
     if (!img){
         ctx.fillStyle = color;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
